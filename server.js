03cc199960a16a9ffb45176314336a2166bc6524
Likhit Kumar V P <likhitkumarvp@Likhits-MacBook-Pro.local>
2022-01-10 23:17:53 +0530

    Author Routes

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 if(process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
+    const dotenv = require('dotenv').config()
+    // const envVariables = Buffer.from('API_KEY=apikey')
+    // const config = dotenv.parse(envVariables) 
+    // console.log(config); // {API_KEY: 'apikey'};
     // not using .parse()
 }
 
 const express = require('express')
 const app = express()
 const expressLayout = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
+
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -15,6 +21,7 @@ app.set('layout', 'layouts/layout')
 
 app.use(expressLayout)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
@@ -22,7 +29,7 @@ const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
-
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
